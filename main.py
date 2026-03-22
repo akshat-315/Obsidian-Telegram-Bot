c64d036b013b40a2c68445cd984d915159b02461
akshat-315 <akshat315.sh@gmail.com>
2026-03-22 17:54:05 +0530

    Created telegram service (high lvl)

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+import logging
 
 from services.logger import setup_logger
+from services.telegram import start_bot
+
+logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     setup_logger()
+    await start_bot()
     yield
+    
 
 
 app = FastAPI(title="Obsidian Workflow Bot", description="A bot to automate workflows in Obsidian", version="1.0.0", lifespan=lifespan)
--- services/telegram.py
+++ services/telegram.py
@@ -1,11 +1,39 @@
 from telegram.ext import Application 
 import logging
 
+from config import settings
+
+logger = logging.getLogger("obsidian_workflow_bot.services.telegram")
+
+_app = Application | None = None
+
 async def start_bot():
-    _app = Application.builder().token("YOUR_TELEGRAM_BOT_TOKEN").build()
+    telegram_api_key = settings.TELEGRAM_API_KEY
 
+    global _app
+    _app = Application.builder().token(telegram_api_key).build()
     
-    await _app.initialize()
-    await _app.start()
-    await _app.updater.start_polling()
+    try:
+        await _app.initialize()
+        await _app.start()
+        await _app.updater.start_polling()
+    except Exception as e:
+        logger.error(f"Error starting Telegram bot: {e}")
+        raise e
 
+    logger.info("Telegram bot started successfully")
+
+async def stop_bot():
+    global _app
+    if _app is None:
+        return None
+    
+    try:
+        await _app.updater.stop()
+        await _app.stop()
+        await _app.shutdown()
+        _app = None
+        logger.info("Telegram bot stopped successfully")
+    except Exception as e:
+        logger.error(f"Error stopping Telegram bot: {e}")
+        raise e
